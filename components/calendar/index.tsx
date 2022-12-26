4b0f300eb5c9ff3bda7814d55a777b8423921fa4
harryakbar <harryakbarntuc@bitbucket.org>
2022-12-26 16:14:56 +0800

    Update calendar: placeholders, date positioning, functionality

--- components/calendar/index.tsx
+++ components/calendar/index.tsx
@@ -1,13 +1,32 @@
 import clsx from "clsx";
 import { useState } from "react";
 
-const generateCalendar = () => {
+const generateCalendar = (month, year) => {
+  const numOfDaysInMonth = new Date(year, month, 0).getDate();
+
   let calendar = [];
-  for (let i = 0; i < 31; i += 1) {
+  for (let i = 0; i < numOfDaysInMonth; i += 1) {
     calendar[i] = i + 1;
   }
   return calendar;
 };
+
+const generatePlaceholderDatesBeforeCurrentMonth = (month, year) => {
+  const startingDayIndex = new Date(year, month - 1, 1).getDay();
+  const numOfDaysInMonth = new Date(year, month - 1, 0).getDate();
+
+  const indexDays = Array.from(Array(startingDayIndex).keys()).reverse();
+  return indexDays.map((day) => numOfDaysInMonth - day);
+};
+
+const generatePlaceholderDatesAfterCurrentMonth = (month, year) => {
+  const numOfDaysInMonth = new Date(year, month, 0).getDate();
+  const lastDayIndex = new Date(year, month - 1, numOfDaysInMonth).getDay() + 1;
+
+  return Array.from(Array(7 - lastDayIndex).keys()).map((day) => day + 1);
+};
+
+const Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const MonthMapping = {
   1: "January",
@@ -26,11 +45,13 @@ const MonthMapping = {
 
 const Calendar: React.FC = () => {
   const [currentMonth, setCurrentMonth] = useState(12);
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
   const [currentDate, setDate] = useState(20);
 
   const handleLeft = () => {
     if (currentMonth - 1 === 0) {
       setCurrentMonth(12);
+      setCurrentYear(currentYear - 1);
     } else {
       setCurrentMonth(currentMonth - 1);
     }
@@ -39,6 +60,7 @@ const Calendar: React.FC = () => {
   const handleRight = () => {
     if (currentMonth + 1 === 13) {
       setCurrentMonth(1);
+      setCurrentYear(currentYear + 1);
     } else {
       setCurrentMonth(currentMonth + 1);
     }
@@ -50,19 +72,42 @@ const Calendar: React.FC = () => {
 
   return (
     <div className="my-8 w-[100%] flex justify-center flex flex-col border-2 rounded-md p-4">
-      <div className="flex flex-row justify-center items-center space-between">
+      <div className="flex flex-row justify-center items-center align-center space-between">
         <button className="w-16 h-16 border-2 rounded-md" onClick={handleLeft}>
           {"<"}
         </button>
-        <article className="prose mx-4">
-          <h1>{MonthMapping[currentMonth]} 2022</h1>
+        <article className="prose mx-4 flex flex-grow justify-center">
+          <h1 className="text-center">
+            {MonthMapping[currentMonth]} {currentYear}
+          </h1>
         </article>
         <button className="w-16 h-16 border-2 rounded-md" onClick={handleRight}>
           {">"}
         </button>
       </div>
       <div className="grid grid-cols-7">
-        {generateCalendar().map((item) => (
+        {Days.map((item) => (
+          <button
+            key="item"
+            className={clsx("flex justify-center center p-2 text-blue-600/100")}
+            onClick={handleClickDate(item)}
+          >
+            {item}
+          </button>
+        ))}
+        {generatePlaceholderDatesBeforeCurrentMonth(
+          currentMonth,
+          currentYear
+        ).map((item) => (
+          <button
+            key="item"
+            className={clsx("flex justify-center center p-2 text-gray-400/100")}
+            onClick={handleClickDate(item)}
+          >
+            {item}
+          </button>
+        ))}
+        {generateCalendar(currentMonth, currentYear).map((item) => (
           <button
             key="item"
             className={clsx(
@@ -70,6 +115,18 @@ const Calendar: React.FC = () => {
               currentDate === item &&
                 "border-none rounded-md bg-sky-500 text-neutral-50"
             )}
+            onClick={handleClickDate(item)}
+          >
+            {item}
+          </button>
+        ))}
+        {generatePlaceholderDatesAfterCurrentMonth(
+          currentMonth,
+          currentYear
+        ).map((item) => (
+          <button
+            key="item"
+            className={clsx("flex justify-center center p-2 text-gray-400/100")}
             onClick={handleClickDate(item)}
           >
             {item}
