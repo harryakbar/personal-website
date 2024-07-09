795d53a34add09580f05eee8ac3f7da5bc967b9f
harryakbarntuc <harry.akbar@ntucenterprise.sg>
2024-07-09 11:38:11 +0800

    Add speed insight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <SpeedInsights />
       <Component {...pageProps} />
       <Analytics />
     </>
