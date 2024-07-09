ebb248da56fc4babe5a0691544f372b12737617b
harryakbarntuc <harry.akbar@ntucenterprise.sg>
2024-07-10 00:01:04 +0800

    Fix preview

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,10 +2,11 @@ import Footer from "./footer";
 import Meta from "./meta";
 
 type Props = {
+  preview?: boolean;
   children: React.ReactNode;
 };
 
-const Layout = ({ children }: Props) => {
+const Layout = ({ preview, children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Meta />
