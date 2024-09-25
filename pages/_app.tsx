325dae83e0b816956e27a4d93ea590274480de0c
harryakbarntuc <harry.akbar@ntucenterprise.sg>
2024-09-25 15:53:48 +0800

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,28 @@ import { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/index.css";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-T0SWJ8NPQB`} // Replace with your Measurement ID
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-T0SWJ8NPQB');
+          `,
+        }}
+      />
       <SpeedInsights />
       <Component {...pageProps} />
       <Analytics />
