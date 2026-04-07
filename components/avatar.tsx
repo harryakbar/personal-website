f1e775e66788221e69726d6aef2239dd1d72c8be
harryakbar <harryakbaram@gmail.com>
2026-04-07 16:01:11 +0800

    feat: Consistently use single quotes for className in Avatar component

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -6,7 +6,7 @@ type Props = {
 const Avatar = ({ name, picture: _picture }: Props) => {
   return (
     <div className="flex items-center">
-      <div className="text-sm font-semibold text-neutral-800">{name}</div>
+      <div className='text-sm font-semibold text-neutral-800'>{name}</div>
     </div>
   );
 };
