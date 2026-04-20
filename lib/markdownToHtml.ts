0a1cf2f7262b393f6a50bea6c4d6fbf1f6d318ff
harryakbar <harryakbaram@gmail.com>
2026-04-20 16:01:48 +0800

    feat: Add lazy loading to images in markdown content

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -3,5 +3,12 @@ import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark().use(html).process(markdown)
-  return result.toString()
+  let processedHtml = result.toString();
+  processedHtml = processedHtml.replace(/<img([^>]+)>/g, (match, p1) => {
+    if (p1.includes('loading=')) {
+      return match;
+    }
+    return `<img${p1} loading="lazy">`;
+  });
+  return processedHtml;
 }
