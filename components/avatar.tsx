62f934260c0f562bf1e19424514b8f2f606f6f59
harryakbar <harryakbaram@gmail.com>
2026-04-21 16:00:27 +0800

    feat: Display author avatar in Avatar component

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,11 +1,23 @@
+import Image from "next/image";
+
 type Props = {
   name: string;
   picture: string;
 };
 
-const Avatar = ({ name, picture: _picture }: Props) => {
+const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
+      <div className="relative mr-4 h-12 w-12">
+        <Image
+          src={picture}
+          className="rounded-full"
+          height={48}
+          width={48}
+          alt={name}
+          priority={false}
+        />
+      </div>
       <div className="text-sm font-semibold text-neutral-800">{name}</div>
     </div>
   );
